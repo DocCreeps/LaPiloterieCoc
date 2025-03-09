9c95c63c8fee90b772fa34ecb301f8cf7c4e2a40
DocCreeps <dorian1998dino@hotmail.com>
2025-03-09 16:52:09 +0100

    clan capital

--- src/components/CapitalDetail.vue
+++ src/components/CapitalDetail.vue
@@ -1,41 +1,70 @@
 <template>
   <!-- Bandeau des Statistiques des Raids de Capital -->
-  <div :class="['p-4 fixed top-0 left-0 right-0 z-10', raidStateClass]">
-    <div class="container mx-auto flex flex-col lg:flex-row justify-between items-center text-white">
-      <!-- Bloc gauche : Infos du Clan -->
-      <div class="flex items-center">
-        <img :src="clan?.badgeUrls?.medium" alt="Clan Badge" class="h-16 w-16 mr-4 cursor-pointer" :key="clan.tag"
-             @click="getClanDetails(clan.tag)">
-        <div>
-          <h1 class="text-2xl font-bold">{{ clan?.name || "Nom du Clan" }}</h1>
-        </div>
-      </div>
-
-      <!-- Bloc central : Infos de la Capitale -->
-      <div class="flex flex-col items-center text-white">
-        <h2 class="text-lg font-bold">Capitale du Clan</h2>
-        <p class="text-sm">Niveau Capital : {{ clan?.clanCapital?.capitalHallLevel || "N/A" }}</p>
-        <img
-          :src="getLeagueIcon(clan?.capitalLeague?.name)"
-          alt="Ligue des Raids"
-          class="mt-2"
-        />
-      </div>
-
-      <!-- Bloc droit : Statistiques des Raids -->
-      <div class="text-right">
-        <h2 class="text-lg font-bold">Statistiques des Raids</h2>
-        <p class="text-sm">État : {{ capitalRaid.state === "ongoing" ? "En cours" : "Terminé" }}</p>
-        <p class="text-sm">Loot Total : {{ capitalRaid.capitalTotalLoot || "N/A" }}</p>
-        <p class="text-sm">Capital Détruite : {{ capitalRaid.raidsCompleted || "N/A" }}</p>
-        <p class="text-sm">Districts Détruits : {{ capitalRaid.enemyDistrictsDestroyed || "N/A" }}</p>
-        <p class="text-sm">Attaques Totales : {{ capitalRaid.totalAttacks || "N/A" }}</p>
+  <div v-if="clan" :class="['p-4 fixed top-0 left-0 right-0 z-10', raidStateClass]">
+    <div class="container mx-auto flex justify-center text-white">
+      <img :src="clan.badgeUrls?.medium" alt="Clan Badge" class="h-16 w-16 mr-4 cursor-pointer" :key="clan.tag"
+           @click="getClanDetails(clan.tag)">
+      <div>
+        <h2 class="text-2xl font-bold">{{ clan.name || "Nom du Clan" }}</h2>
+        <h1 class="text-2xl font-bold">Capital de clan</h1>
       </div>
     </div>
   </div>
 
-  <!-- Contenu Principal -->
-  <div class="flex justify-center py-16 mt-24">
+  <!-- Carte de la Capitale -->
+  <div class="container mx-auto py-16 mt-24">
+    <div class="bg-white p-6 rounded-lg shadow-lg mx-auto mb-8 max-w-4xl text-center">
+      <div class="flex flex-col items-center mb-4">
+        <img
+          :src="getLeagueIcon(clan?.capitalLeague?.name)"
+          alt="Ligue des Raids"
+          class="h-12 w-12 mb-2"
+        />
+        <h2 class="text-xl font-bold">Détails des Raids de Capitale</h2>
+      </div>
+      <p class="text-sm"><strong>Niveau Capitale :</strong> {{ clan?.clanCapital?.capitalHallLevel || "N/A" }}</p>
+      <div v-if="clan?.clanCapital?.districts && clan.clanCapital.districts.length" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div v-for="(district, index) in clan.clanCapital.districts" :key="index" class="p-4 border rounded bg-gray-100 text-center">
+          <p class="text-sm">
+            <strong>{{ district.name || "N/A" }} :</strong> {{ district.districtHallLevel || "N/A" }}
+          </p>
+        </div>
+      </div>
+
+
+
+    </div>
+
+    <!-- Sélecteur de Raids -->
+    <div class="bg-gray-50 p-4 rounded-lg shadow-md mx-auto mb-8 max-w-4xl">
+      <label for="raidSelect" class="block text-lg font-bold mb-2">Sélectionnez un Raid :</label>
+      <select
+        id="raidSelect"
+        v-model="selectedRaidIndex"
+        @change="updateSelectedRaid"
+        class="w-full p-2 border rounded-md"
+      >
+        <option
+          v-for="(raid, index) in raids"
+          :key="index"
+          :value="index"
+        >
+          Raids du {{ formatDate(raid.startTime) }} au {{ formatDate(raid.endTime) }}
+        </option>
+      </select>
+    </div>
+
+    <!-- Informations du Raid Sélectionné -->
+    <div v-if="selectedRaid" class="bg-white p-6 rounded-lg shadow-lg mx-auto mb-8 max-w-4xl">
+      <h2 class="text-lg font-bold">Informations du Raid Sélectionné</h2>
+      <p class="text-sm"><strong>Raids du :</strong> {{ formatDate(selectedRaid.startTime) }} au {{ formatDate(selectedRaid.endTime) }}</p>
+      <p class="text-sm"><strong>Loot Total :</strong> {{ selectedRaid.capitalTotalLoot || "N/A" }}</p>
+      <p class="text-sm"><strong>Raids Complétés :</strong> {{ selectedRaid.raidsCompleted || "N/A" }}</p>
+      <p class="text-sm"><strong>Districts Détruits :</strong> {{ selectedRaid.enemyDistrictsDestroyed || "N/A" }}</p>
+      <p class="text-sm"><strong>Attaques Totales :</strong> {{ selectedRaid.totalAttacks || "N/A" }}</p>
+    </div>
+  </div>
+  <div v-if="selectedRaid.state === 'ongoing'" class="flex justify-center py-16 mt-24">
     <div class="w-full max-w-4xl bg-white p-4 rounded-lg shadow-md flex space-x-8">
       <div class="w-1/2">
         <h2 class="text-xl font-bold mb-4 text-center">Membres avec Attaques</h2>
@@ -60,8 +89,8 @@
       </div>
     </div>
   </div>
-</template>
 
+</template>
 
 <script>
 import apiService from '../apiService';
@@ -70,41 +99,27 @@ export default {
   data() {
     return {
       clan: null, // Détails du clan
-      capitalRaid: {}, // Détails des raids de capitale
-      members: [], // Membres participant aux raids
-      clanMembers: [], // Membres du clan
+      raids: [], // Liste des raids de capitale
+      selectedRaidIndex: null, // Index du raid sélectionné
+      selectedRaid: {}, // Données du raid sélectionné
       leagues: [], // Liste des ligues disponibles
       unrankedLeagueIcon: '', // Icône pour "Non classé"
-      showZeroAttackList: false, // Indicateur d'affichage de la liste déroulante
-      isLoading: false, // Indicateur de chargement
+      raidStateClass: 'bg-green-500', // Classe par défaut pour raidStateClass
+      clanMembers: [], // Membres du clan
     };
   },
   created() {
     this.fetchClanDetails();
-    this.fetchClanMembers();
     this.fetchCapitalRaidDetails();
-    this.fetchLeagues(); // Récupération des données de ligues pour les icônes
-  },
-  mounted() {
-    // Définit dynamiquement le titre de la page
-    document.title = `Détail des raids Capital du clan - ${this.clan?.name || 'Chargement...'}`;
+    this.fetchLeagues();
+    this.fetchClanMembers();
   },
   methods: {
-    fetchClanMembers() {
-      const clanTag = this.$route.params.clanTag;
-      return apiService.getClanDetails(clanTag).then(response => {
-        this.clanMembers = response.memberList;
-      }).catch(error => {
-        console.error("Erreur lors de la récupération des membres du clan :", error);
-      });
-    },
     // Récupère les détails du clan
     fetchClanDetails() {
       const clanTag = this.$route.params.clanTag;
       apiService.getClanDetails(clanTag).then(response => {
         this.clan = response;
-        // Met à jour le titre une fois les données récupérées
-        document.title = `Détail des raids Capital du clan - ${this.clan.name}`;
       }).catch(error => {
         console.error('Erreur lors de la récupération des détails du clan :', error);
       });
@@ -113,17 +128,19 @@ export default {
     fetchCapitalRaidDetails() {
       const clanTag = this.$route.params.clanTag;
       apiService.getCapitalDetails(clanTag).then(response => {
-        this.capitalRaid = response.items[0];
-        this.members = this.capitalRaid.members || [];
+        this.raids = response.items || [];
+        if (this.raids.length > 0) {
+          this.selectedRaidIndex = 0; // Par défaut, le premier raid
+          this.updateSelectedRaid();
+        }
       }).catch(error => {
-        console.error('Erreur lors de la récupération des détails des raids de capital :', error);
+        console.error('Erreur lors de la récupération des raids de capital :', error);
       });
     },
     // Récupère la liste des ligues pour les icônes
     fetchLeagues() {
       apiService.getLeagues().then(response => {
         this.leagues = response.items;
-        // Définir l'icône par défaut pour "Non classé"
         const unrankedLeague = this.leagues.find(league => league.name === 'Unranked');
         if (unrankedLeague) {
           this.unrankedLeagueIcon = unrankedLeague.iconUrls.medium;
@@ -132,25 +149,59 @@ export default {
         console.error('Erreur lors de la récupération des ligues :', error);
       });
     },
+    // Mets à jour le raid sélectionné
+    updateSelectedRaid() {
+      this.selectedRaid = this.raids[this.selectedRaidIndex] || {};
+    },
+    // Formate une date
+    formatDate(dateString) {
+      if (!dateString) {
+        return "Date inconnue";
+      }
+      try {
+        // Reformate la chaîne de date pour inclure les délimiteurs nécessaires
+        const formattedString = dateString.replace(
+          /^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})(\.\d+)?Z$/,
+          "$1-$2-$3T$4:$5:$6Z"
+        );
+        const date = new Date(formattedString);
+
+        // Vérifie si la date est valide
+        if (isNaN(date)) {
+          throw new Error("Date invalide");
+        }
+
+        // Retourne une date formatée pour l'affichage
+        return date.toLocaleDateString("fr-FR", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+      } catch (error) {
+        console.error("Date invalide :", dateString);
+        return "Date inconnue";
+      }
+    },
     // Récupère l'icône correspondant au nom d'une ligue
     getLeagueIcon(leagueName) {
       const league = this.leagues.find(league => league.name === leagueName);
       return league?.iconUrls?.small || this.unrankedLeagueIcon;
     },
-    // Gère l'affichage/masquage de la liste des membres à 0 attaques
-    toggleZeroAttackList() {
-      this.showZeroAttackList = !this.showZeroAttackList;
-    },
-    getClanDetails(clanTag) {
-      const cleanedClanTag = clanTag.replace('#', '');
-      this.$router.push(`/clan/${cleanedClanTag}`);
+    // Récupère les membres du clan
+    fetchClanMembers() {
+      const clanTag = this.$route.params.clanTag;
+      return apiService.getClanDetails(clanTag).then(response => {
+        this.clanMembers = response.memberList;
+      }).catch(error => {
+        console.error("Erreur lors de la récupération des membres du clan :", error);
+      });
     },
   },
   computed: {
     // Fusion des données des membres de clan et de raids
     enrichedMembers() {
       const mergedMembers = this.clanMembers.map(clanMember => {
-        const raidMember = this.members.find(member => member.tag === clanMember.tag);
+        const raidMember = this.selectedRaid.members?.find(member => member.tag === clanMember.tag);
         return raidMember || {
           tag: clanMember.tag,
           name: clanMember.name,
@@ -172,25 +223,22 @@ export default {
       return this.enrichedMembers.filter(member => member.attacks === 0);
     },
     // Retourne la classe CSS pour l'état des raids (en cours ou terminé)
-    raidStateClass() {
-      return this.capitalRaid.state === "ongoing" ? 'bg-green-500' : 'bg-red-500';
-    },
+
   },
 };
 </script>
-
 
 
 <style scoped>
 .bg-green-500 {
   background-color: #48bb78; /* Vert pour les raids en cours */
 }
+
 .bg-red-500 {
   background-color: #f56565; /* Rouge pour les raids terminés */
 }
+
 .text-white {
   color: #ffffff;
 }
-
 </style>
-
